eb4c30928c1d2a63bc3bcd8dd741b06e7fc636e8
liuyunjs <429695034@qq.com>
2021-07-11 18:53:20 +0800

    PortalProvider 新增 Portal 包裹，让静态调用时view渲染到同级

--- src/PortalProvider.tsx
+++ src/PortalProvider.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { PortalPatcher } from './PortalPatcher';
+import { Portal } from './Portal';
 
 export const PortalProvider: React.FC = ({ children }) => {
   return (
     <>
       {children}
-      <PortalPatcher />
+      <Portal>
+        <PortalPatcher />
+      </Portal>
     </>
   );
 };
