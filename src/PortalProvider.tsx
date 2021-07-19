4ff9b623ec88522b6dad12903d4dd366cb1a73ce
liuyunjs <429695034@qq.com>
2021-07-19 20:47:48 +0800

    PortalProvider 取消 Portal

--- src/PortalProvider.tsx
+++ src/PortalProvider.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { PortalPatcher } from './PortalPatcher';
-import { Portal } from './Portal';
 
 export const PortalProvider: React.FC = ({ children }) => {
   return (
     <>
       {children}
-      <Portal>
-        <PortalPatcher />
-      </Portal>
+      <PortalPatcher />
     </>
   );
 };
