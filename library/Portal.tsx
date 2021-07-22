507ea78cf1877d46587fe0471aa388ec62ef2516
liuyunjs <429695034@qq.com>
2021-07-22 22:14:52 +0800

    因为createPortal有bug，所以Portal 默认开启 legacy

--- library/Portal.tsx
+++ library/Portal.tsx
@@ -3,14 +3,10 @@ import { useRootTag } from './useRootTag';
 import { getCreatePortal } from './createPortal';
 import { LegacyPortal, LegacyPortalProps } from './LegacyPortal';
 
-export type PortalProps =
-  | {
-      children: React.ReactElement;
-      legacy?: false;
-    }
-  | ({
-      legacy: true;
-    } & LegacyPortalProps);
+export type PortalProps = {
+  children: React.ReactElement;
+  legacy?: boolean;
+} & LegacyPortalProps;
 
 export const Portal: React.FC<PortalProps> = (props) => {
   const rootTag = useRootTag();
@@ -19,4 +15,8 @@ export const Portal: React.FC<PortalProps> = (props) => {
   }
   const createPortal = getCreatePortal();
   return createPortal(props.children, rootTag);
+};
+
+Portal.defaultProps = {
+  legacy: true,
 };
